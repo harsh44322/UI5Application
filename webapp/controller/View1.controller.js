7f333c32d0053690f5d05d1e59c04c180b7c0380
harsh44322 <harsh88588@gmail.com>
2024-07-14 16:07:23 +0530

    Added Logic to get data for the clicked object in second view

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,19 +1,34 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("project1.controller.View1", {
             onInit: function () {
-
+                var oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData(this.getOwnerComponent().getModel("employee").getData());
+                sap.ui.getCore().setModel(oModel, 'oModel');
             },
-            onNav: function(){
-                var oRouter = this.getOwnerComponent().getRouter(this);
-                oRouter.navTo("detail");
+            onNav: function(oEvent){
+                var oItem, oCtx;
+                oItem = oEvent.getSource();
+                oCtx = oItem.getBindingContext("employee");
+                var oIndexData = oCtx.getProperty();
+                var EmpDetail = new sap.ui.model.json.JSONModel();
+                EmpDetail.setData(oIndexData);
+                sap.ui.getCore().setModel(EmpDetail, 'EmpDetail');
+                this.getOwnerComponent().getRouter().navTo("detail");
+                // var oItem = oEvent.getSource();
+                // var sEmployeeId = oItem.getBindingContext().getProperty("employeeId");
+            
+                // this.getOwnerComponent().getRouter().navTo("detail", {
+                //     id: sEmployeeId
+                // });
             }
         });
     });
--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -1,15 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("project1.controller.View2", {
             onInit: function () {
-
-            }
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			    oRouter.getRoute("detail").attachMatched(this._onRouteMatched, this);
+            },
+            _onRouteMatched: function(oEvent){
+                var newModel = new sap.ui.model.json.JSONModel();
+                newModel.setData(sap.ui.getCore().getModel("EmpDetail").getData());
+                sap.ui.getCore().setModel(newModel,'newModel');
+                this.getOwnerComponent().setModel(newModel,'newModel');
+                
+            },
+            _onBindingChange: function(oEvent){
+                if(!this.getView().getBindingContext()){
+                    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                    oRouter.getTarget();
+                }
+            },
         });
     });
