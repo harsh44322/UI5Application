657c096669d60fafb496b0381b01338b8afa6951
harsh44322 <harsh88588@gmail.com>
2024-07-14 18:26:12 +0530

    Added JS logic to update vizframe

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -12,6 +12,7 @@ sap.ui.define([
             onInit: function () {
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			    oRouter.getRoute("detail").attachMatched(this._onRouteMatched, this);
+                this.onInitVizFrame();
             },
             _onRouteMatched: function(oEvent){
                 var newModel = new sap.ui.model.json.JSONModel();
@@ -26,5 +27,45 @@ sap.ui.define([
                     oRouter.getTarget();
                 }
             },
+            onInitVizFrame: function(){
+                var oVizFrame = this.getView().byId("idForPerformanceChart");
+                var oDataset = new sap.viz.ui5.data.FlattenedDataset({
+                    dimension : [{
+                        name: 'Year',
+                        value: "{newModel>/year}"
+                    }],
+                    measures : [{
+                        name: 'Feedback Point',
+                        value: "{newModel>/score}"
+                    }],
+                    data : {
+                        path : "newModel>/employees/performance"
+                    }
+                });
+                oVizFrame.setDataset(oDataset);
+                oVizFrame.setVizType('bar');
+
+                oVizFrame.setVizProperties({
+                    plotArea: {
+                        colorPalette: d3.scale.category20().range()
+                    },
+                    title: {
+                        visible: true,
+                        text: "Performance Chart"
+                    }
+                });
+                var feedValueAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
+                    'uid':"valueAxis",
+                    'type':"Measure",
+                    'values':["Feedback Point"]
+                }),
+                feedCatogeryAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
+                    'uid':"categoryAxis",
+                    'type':"Dimension",
+                    'values':["Year"]
+                });
+                oVizFrame.addFeed(feedValueAxis);
+                oVizFrame.addFeed(feedCatogeryAxis);
+            }
         });
     });
