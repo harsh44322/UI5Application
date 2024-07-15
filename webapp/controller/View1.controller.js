862b8c137444c45183a435bda8c8f757275ca14b
harsh44322 <harsh88588@gmail.com>
2024-07-15 19:24:59 +0530

    Made chandges in searchfield

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, Filter, FilterOperator, Fragment) {
         "use strict";
 
         return Controller.extend("project1.controller.View1", {
@@ -23,12 +26,32 @@ sap.ui.define([
                 EmpDetail.setData(oIndexData);
                 sap.ui.getCore().setModel(EmpDetail, 'EmpDetail');
                 this.getOwnerComponent().getRouter().navTo("detail");
-                // var oItem = oEvent.getSource();
-                // var sEmployeeId = oItem.getBindingContext().getProperty("employeeId");
-            
-                // this.getOwnerComponent().getRouter().navTo("detail", {
-                //     id: sEmployeeId
-                // });
+            },
+            onLiveChange: function(oEvent){
+                // var aFilter = [];
+                // var sQuery = oEvent.getSource().getValue();
+                // if(sQuery){
+                //     aFilter.push(new Filter("lastName", FilterOperator.Contains, sQuery));
+                // }
+                // var oTab = this.getView().byId("idForTreeTable");
+                // var oBinding = oTab.getBinding("items");
+                // oBinding.filter(aFilter);
+                var that = this;
+
+                var sQuery = oEvent.getParameter("newValue"); // Use newValue to capture the user's input
+                var oTab = sap.ui.core.Fragment.byId("idForTreeTable");
+                var oTable = this.byIdFragment("idForTreeTable");
+            var oBinding = oTable.getBinding("row");
+            var aFilters = [];
+
+            if (sQuery && sQuery.length > 0) {
+                // Create a filter for the lastName field
+                var oFilter = new Filter("firstName", FilterOperator.Contains, sQuery);
+                aFilters.push(oFilter);
+            }
+
+            // Apply the filter to the binding
+            oBinding.filter(aFilters);
             }
         });
     });
